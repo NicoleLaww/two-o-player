9962e570f8bdd0a02342a865e12b9f1da9f3a739
Nicole Law <nicole.law12@gmail.com>
2023-07-25 23:52:32 -0400

    completed

---
+++ scores.rb
@@ -0,0 +1,23 @@
+# Don't need 
+# class Score
+#   attr_accessor :score, :player_id
+
+#   def initialize(player_id)
+#     @score = 3   
+#     @player_id = player_id
+#   end
+
+#   def update_score(boolean)
+#     if boolean == false 
+#       @score -= 1 
+#     end 
+#   end 
+
+#   def get_score()
+#     @score
+#   end 
+    
+# end 
+
+
+
